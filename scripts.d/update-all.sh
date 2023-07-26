4841a927c6d268918e03074440c87bffd03f2d22
brandon <brandon@smith.net>
2023-07-26 23:29:28 +0100

    Autocommit from 2023-07-26 23:29:28
    Klipper on commit: d725dfd
    
    Moonraker on commit: aebcd00
    
    Mainsail version: v2.6.1

--- scripts.d/update-all.sh
+++ scripts.d/update-all.sh
@@ -2,17 +2,30 @@ sudo service klipper stop
 cd ~/klipper
 git pull
 
+#MCU
 make clean KCONFIG_CONFIG=config.btt-skr-mini-e3-v2
 make menuconfig KCONFIG_CONFIG=config.btt-skr-mini-e3-v2
 make KCONFIG_CONFIG=config.btt-skr-mini-e3-v2
+#make flash FLASH_DEVICE=/dev/serial/by-id/usb-Klipper_stm32f103xe_37FFD5055358353227800843-if00 KCONFIG_CONFIG=config.btt-skr-mini-e3-v2
 ./scripts/flash-sdcard.sh /dev/serial/by-id/usb-Klipper_stm32f103xe_37FFD5055358353227800843-if00 btt-skr-mini-e3-v2 
 
+#linuxhost
 make clean KCONFIG_CONFIG=config.rpi
 make menuconfig KCONFIG_CONFIG=config.rpi
 make flash KCONFIG_CONFIG=config.rpi
 
-sudo service klipper start
+#toolhead
+make clean 
+make menuconfig 
+make 
+python3 ~/CanBoot/scripts/flash_can.py -i can0 -f ~/klipper/out/klipper.bin -u 6015553193ee
 
-#make flash FLASH_DEVICE=0483:df11 
-#display serial: /dev/serial/by-id/usb-Klipper_stm32f042x6_0A0036000C43535035373520-if00
+#V0Display / expander
+#display serial: /dev/serial/by-id/usb-Klipper_stm32f042x6_0A0036000C43535035373520-if00 DFU [0483:df11]
 #expander serial: /dev/serial/by-id/usb-Klipper_stm32f042x6_280014000A43564636373420-if00 DFU [0483:df11]
+make clean 
+make menuconfig 
+make
+make flash FLASH_DEVICE=0483:df11 
+
+sudo service klipper start
